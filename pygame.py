25a076c2cf5bff6d726b5e50ae3349fe6831d00a
korn3lia <64958573+korn3lia@users.noreply.github.com>
2021-05-23 15:27:46 +0200

    wczytanie obrazu tła

--- pygame.py
+++ pygame.py
@@ -1,7 +1,11 @@
 #okono gry w gui:
-
 from  tkinter import
 okno_gry = Tk()
 okno_gry.title("Gra zamkowe sprzataczki")
 okno_gry.geometry("350x300")
 okno_gry.mainloop()
+
+#wczytanie obrazu tła
+background_image=tk.PhotoImage('lava.jpg')
+background_label=tk.Label(parent, image=background_image)
+background_label.place=(x=0, y=0, relwidth=1, relheight=1)
