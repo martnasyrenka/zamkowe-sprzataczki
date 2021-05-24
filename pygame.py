14df9dbe8a1d2731bd1eb5cfc2461ce79159d57b
korn3lia <64958573+korn3lia@users.noreply.github.com>
2021-05-24 20:23:56 +0200

    wczytanie obrazu protagonisty gry

--- pygame.py
+++ pygame.py
@@ -11,5 +11,10 @@ background_image=Image.open('lava.jpg')
 background_imageTk=ImageTk.Photoimage(background_image)
 plotno.create_image(350, 200,image=background_imageTk)
 
+#wczytanie grafiki protagonisty gry
+protag_image=Image.open('protag.png')
+protag_imageTk=ImageTk.Photoimage(protag_image)
+plotno.create_image(20, 8,image=protag_imageTk)
+
 
 okno_gry.mainloop()
