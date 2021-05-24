8ec8d48f6f0cb106083fb421619f1f4efb09d224
korn3lia <64958573+korn3lia@users.noreply.github.com>
2021-05-24 19:59:30 +0200

    Update pygame.py

--- pygame.py
+++ pygame.py
@@ -3,10 +3,13 @@ from  tkinter import
 okno_gry = Tk()
 okno_gry.title("Gra zamkowe sprzataczki")
 okno_gry.geometry("350x300")
-okno_gry.mainloop()
+
 
 #wczytanie obrazu tła
 plotno.pack()
 background_image=Image.open('lava.jpg')
 background_imageTk=ImageTk.Photoimage(background_image)
 plotno.create_image(350, 200,image=background_imageTk)
+
+
+okno_gry.mainloop()
