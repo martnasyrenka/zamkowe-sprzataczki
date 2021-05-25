eededa88bba182cc802f18493946e48534544196
PattiiiL <64958605+PattiiiL@users.noreply.github.com>
2021-05-25 16:04:48 +0200

    Update pygame.py

--- pygame.py
+++ pygame.py
@@ -1,8 +1,42 @@
-#okono gry w gui:
-from  tkinter import
+'''___________________________________________________________________________
+Wszystkie importy'''
+
+from Tkinter import*
+from PIL import Image, ImageTk
+
+import pygame
+import random
+
+
+'''___________________________________________________________________________
+Wszystkie funkcje'''
+
+
+puzzle = ""
+
+
+def losuj_puzzle:
+    ilosc_puzzli = 6
+    global wylosowane_puzzle
+    wylosowane_puzzle = []
+    for i in range(ilosc_puzzli):
+        wylosowane_puzzle+= random.choice(puzzle)
+        
+
+def wyswietl_wylosowane_puzzle:
+    print(wylosowane_puzzle)
+
+def dodaj_jeden:
+    if len(wylosowane_puzzle)<6:
+        wylosowane_puzzle.aapend(random.choice(puzzle))
+
+'''___________________________________________________________________________
+okono gry'''
+
+
 okno_gry = Tk()
-okno_gry.title("Gra zamkowe sprzataczki")
-okno_gry.geometry("350x300")
+okno_gry.title("Zamkowe sprzataczki - gra puzzle")
+okno_gry.geometry("350x200")
 
 
 #wczytanie obrazu tła
@@ -16,5 +50,7 @@ protag_image=Image.open('protag.png')
 protag_imageTk=ImageTk.Photoimage(protag_image)
 plotno.create_image(20, 8,image=protag_imageTk)
 
+przycisk_losuj_puzzle = Button(okno_gry, text="Losuj puzle",fg="red",command = losuj_puzzle, command = wyswietl_wylosowane_puzzle, command= dodaj_jeden)
+przycisk_losuj_puzzle.pack(side=BOTTOM)
 
 okno_gry.mainloop()
