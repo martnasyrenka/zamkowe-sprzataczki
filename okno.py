cd29c7ae06784e9f702d54c72876721a70c25534
Kornelia Winiarska <korwin@st.amu.edu.pl>
2021-05-31 20:28:45 +0200

    wczytanie grafiki puzzli

--- okno.py
+++
@@ -1,5 +0,0 @@
-from  tkinter import
-okno_gry = Tk()
-okno_gry.title("Gra zamkowe sprzataczki")
-okno_gry.geometry("350x300")
-okno_gry.mainloop()
--- pygame.py
+++ pygame.py
@@ -19,20 +19,32 @@ okno_gry.geometry("600x600")
 
 
 #wczytanie obrazu tła
-
 tlo_zamek=PhotoImage(file="graphics/background/castle.png")
 tlo=Label(okno_gry, image=tlo_zamek)
 tlo.place(x=0,y=0)
 
+#wczytanie obrazów puzzli
+puzzle_prawodol_zdj=PhotoImage(file='graphics/path/prawo_dol.png')
+puzzle_prawodol=Label(okno_gry, image=puzzle_prawodol_zdj)
+puzzle_prawodol.place(x=190, y=100) 
+
+puzzle_prawogora_zdj=PhotoImage(file='graphics/path/prawo_gora.png')
+puzzle_prawogora=Label(okno_gry, image=puzzle_prawogora_zdj)
+puzzle_prawogora.place(x=190, y=150)
+
+puzzle_lewogora_zdj=PhotoImage(file='graphics/path/lewo_gora.png')
+puzzle_lewogora=Label(okno_gry, image=puzzle_lewogora_zdj)
+puzzle_lewogora.place(x=190, y=200)
 
 puzzle_prosto_zdj=PhotoImage(file='graphics/path/prosto.png')
 puzzle_prosto=Label(okno_gry, image=puzzle_prosto_zdj)
-puzzle_prosto.place(x=0, y=500)
+puzzle_prosto.place(x=190, y=250)
 
 puzzle_lewodol_zdj=PhotoImage(file='graphics/path/lewo_dol.png')
 puzzle_lewodol=Label(okno_gry, image=puzzle_lewodol_zdj)
-puzzle_lewodol.place(x=60, y=500)
+puzzle_lewodol.place(x=190, y=300)
 
+#wczytanie grafiki protagonsty
 protag_zdj=PhotoImage(file='graphics/protagonist/protag.png')
 protag=Label(okno_gry, image=protag_zdj)
 protag.place(x=200, y=400)
