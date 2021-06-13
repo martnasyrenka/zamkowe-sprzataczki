b2cf364ae08e0a4c383d9afb9b2cb722d56428d8
martnasyrenka <83359749+martnasyrenka@users.noreply.github.com>
2021-06-13 12:32:37 +0200

    move counter 2 szkielet

---
+++ movecounter2.py
@@ -0,0 +1,13 @@
+pygame.font.init()
+font = pygame.font.Font('freesansbold.ttf', 25)
+#licznik ruchów
+move_value = 0
+
+mvX = 350 #(POŁOŻENIE MOVE COUNTERA X - DO DOSTOSOWANIA)
+mvY = 300 #(POŁOŻENIE MOVE VOUNTERA Y - DO DOSTOSOWANIA)
+
+def how_much_moves(x, y):
+    move_counter = font.render("Moves:" + " " + str(move_value), True, (225, 225, 225))
+    screen.blit(move_counter, (x, y))
+    
+#update'y move countera w poszczególnych patchach do funkcji/pętli
